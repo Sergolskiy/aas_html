d5aeb3b3af37526053a4efd48676e0fb0768e5d7
DarthJktu <DarthJktu@gmail.com>
2020-01-14 17:28:46 +0200

    2

--- js/account.js
+++ js/account.js
@@ -12,4 +12,21 @@ $(document).ready(function () {
 		val = parseInt(val) + 1;
 		input.val(new Intl.NumberFormat('en-EN').format(val))
 	});
+
+	$('.a-contact__phone-country').click(function () {
+		$(this).find('.a-contact__phone-list').toggleClass('open');
+	});
+
+	$('.a-contact__phone-item').click(function () {
+		var html =  $(this).html();
+		$('.a-contact__phone-current').html(html);
+	});
+
+	$('.a-contact__input-checkbox-wrap input').change(function () {
+		if($(this).prop('checked') !== true){
+			$('.a-contact__physical-address').addClass('a-contact__physical-address--open');
+		} else {
+			$('.a-contact__physical-address').removeClass('a-contact__physical-address--open');
+		}
+	});
 });
